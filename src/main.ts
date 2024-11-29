1ec601300516d0df6a91d1ebf044f3dbad21f058
RicardoSG <rs795384@hotmail.com>
2024-11-28 22:03:59 -0600

    Enabled Cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: 'http://35.153.203.58',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
   await app.listen(3000);
 }
 bootstrap();
